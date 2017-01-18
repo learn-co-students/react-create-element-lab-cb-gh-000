8584bb69383d3557e23875314cd25ff3f71cd798
Joakim Järvinen <georgepatton1945@hotmail.com>
2017-01-18 15:06:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //Code React element here
+const list =
+  React.createElement('div', {class:'me'},
+    React.createElement('h1', {}, 'An Awesome Person'),
+    React.createElement('p', {}, 'Who is learning React'),
+    React.createElement('ul', {class:'me__interests'},
+      [
+        React.createElement('li', {}, 'JavaScript'),
+        React.createElement('li', {}, 'React'),
+        React.createElement('li', {}, 'Movies'),
+        React.createElement('li', {}, 'ce cream'),
+      ]
+    ));
+
+ReactDOM.render(list, document.getElementById('main'));
