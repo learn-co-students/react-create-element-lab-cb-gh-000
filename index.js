e30063f7bc850520fa810e28fde5d59e2492600b
Rahul Sai <rahulsharma7651@gmail.com>
2017-05-18 10:08:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Code React element here
+const title = React.createElement('h1', {}, 'An Awesome Person');
+const paragraph = React.createElement('p', {}, 'Who is learning React');
+
+const javascript = React.createElement('li', {}, 'JavaScript');
+const react = React.createElement('li', {}, 'React');
+const movies = React.createElement('li', {}, 'Movies');
+const icecream = React.createElement('li', {}, 'Ice cream');
+const interests = React.createElement('ul', {className: 'me__interests'}, javascript, react, movies, icecream);
+
+const meInReact = React.createElement('div', {className: 'me'}, [title, paragraph, interests]);
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+);
