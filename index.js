5270cc6654a342c3f4d6f66595645691f657b0ac
Adrián Daraš <getpingui@gmail.com>
2017-05-26 16:51:24 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Code React element here
+const meInReact = React.createElement("div", { className: "me" }, [
+  React.createElement("h1", null, "An Awesome Person"),
+  React.createElement("p", null, "Who is learning React"),
+  React.createElement("ul", { className: "me__interests" }, [
+    React.createElement("li", null, "JavaScript"),
+    React.createElement("li", null, "React"),
+    React.createElement("li", null, "Movies"),
+    React.createElement("li", null, "Ice cream")
+  ]),
+]);
