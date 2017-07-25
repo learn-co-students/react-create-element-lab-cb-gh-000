1c693fe10f9aac1f0bba205d23765660c3e885c7
Neel B <neelster@gmail.com>
2017-07-25 22:43:39 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 
-const header = React.createElement('h1', {}, "Neel Bhoopalam");
-const sub_description = React.createElement('p', {}, "is testing out Flatiron School");
+const header = React.createElement('h1', {}, "An Awesome Person");
+const sub_description = React.createElement('p', {}, "Who is learning React");
 const list = React.createElement('ul', {className: 'my-interests'},
                                           [
-                                            React. createElement('li', {}, 'Tennis'),
+                                            React. createElement('li', {}, 'JavaScript'),
+                                            React. createElement('li', {}, 'React'),
                                             React. createElement('li', {}, 'Movies'),
-                                            React. createElement('li', {}, 'Soccer')
+                                            React. createElement('li', {}, 'Ice cream'),
                                           ]
                                 );
 
