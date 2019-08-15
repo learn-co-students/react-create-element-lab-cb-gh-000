3dcb351ba4a720d44f72b457d839f53849acd5b4
N M <niclas@mettenleiter.com>
2019-08-15 11:44:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const meInReact = React.createElement('div', { className: 'me'},
   React.createElement('h1', {}, 'An Awesome Person'),
   React.createElement('p', {}, 'Who is learning React'),
-  React.createElement('ul', { className: 'my-interests' }, 
+  React.createElement('ul', { className: 'my-interests' },
     React.createElement('')
 
 
