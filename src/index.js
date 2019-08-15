f30ee9b300a8c652ad1208ec74db47d961b1db9e
N M <niclas@mettenleiter.com>
2019-08-15 11:45:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,22 +5,11 @@ const meInReact = React.createElement('div', { className: 'me'},
   React.createElement('h1', {}, 'An Awesome Person'),
   React.createElement('p', {}, 'Who is learning React'),
   React.createElement('ul', { className: 'my-interests' },
-    React.createElement('')
-
-
+    React.createElement('li', {}, 'nix')
+  )
 )
-  <div class="me">
-  <h1>An Awesome Person</h1>
-  <p>Who is learning React</p>
 
-  <ul class="my-interests">
-    <li>JavaScript</li>
-    <li>React</li>
-    <li>Movies</li>
-    <li>Ice cream</li>
-  </ul>
-</div>
-)
+
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 
