c8c1488fd1c3ce18746c819a071170fc3f459d4d
N M <niclas@mettenleiter.com>
2019-08-15 11:45:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const meInReact = React.createElement('div', { className: 'me'},
   React.createElement('h1', {}, 'An Awesome Person'),
   React.createElement('p', {}, 'Who is learning React'),
   React.createElement('ul', { className: 'my-interests' },
-    React.createElement('li', {}, 'nix')
+    React.createElement('li', {}, 'nix'),
+    React.createElement('li', {}, 'nix'),
+    React.createElement('li', {}, 'nix'),
+    React.createElement('li', {}, 'nix'),
   )
 )
 
